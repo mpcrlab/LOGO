d8fc063d89c7bf05d01934ad7548e63f9061345e
root <mtetifau@gmail.com>
2018-04-24 18:06:52 +0000

    added argument for autonomy

--- drive.py
+++ drive.py
@@ -1,4 +1,10 @@
 from RoverBrain import *
+import argparse
+
+parser = argparse.ArgumentParser(description='human driver')
+parser.add_argument('--driver')
+args = parser.parse_args()
+print(args.driver)
 
 if __name__ == '__main__':
-    rover = RoverBrain()
+    rover = RoverBrain(args.driver)
