d817d36418876b650834e8169f4f477c24b04ebb
root <mtetifau@gmail.com>
2018-04-26 21:39:06 +0000

    added argument to save dicts

--- drive.py
+++ drive.py
@@ -1,10 +1,12 @@
 from RoverBrain import *
 import argparse
 
-parser = argparse.ArgumentParser(description='human driver')
+parser = argparse.ArgumentParser(description='Rover Curiosity Parameters')
 parser.add_argument('--driver')
+parser.add_argument('--save_dict')
 args = parser.parse_args()
 print(args.driver)
+print(args.save_dict)
 
 if __name__ == '__main__':
-    rover = RoverBrain(args.driver)
+    rover = RoverBrain(args.driver, args.save_dict)
